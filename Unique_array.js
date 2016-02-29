5c7771fef5bf67255313383c61e9e682f12bbf8f
zaki <zakizak2016@yahoo.com>
2016-02-29 22:31:46 +0700

    Create Unique_array.js

---
+++ Unique_array.js
@@ -0,0 +1,18 @@
+function unique(obj){
+    var uniques=[];
+    var stringify={};
+    for(var i=0;i<obj.length;i++){
+       var keys=Object.keys(obj[i]);
+       keys.sort(function(a,b) {return a-b});
+       var str='';
+        for(var j=0;j<keys.length;j++){
+           str+= JSON.stringify(keys[j]);
+           str+= JSON.stringify(obj[i][keys[j]]);
+        }
+        if(!stringify.hasOwnProperty(str)){
+            uniques.push(obj[i]);
+            stringify[str]=true;
+        }
+    }
+    return uniques;
+}
